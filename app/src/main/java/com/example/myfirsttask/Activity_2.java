24c29ae35e6e2c2cdb68cf89520987869d7df067
Boyka89cid <kushaldevgun@gmail.com>
2021-06-09 01:36:37 +0530

    Both Layouts Together

---
+++ app/src/main/java/com/example/myfirsttask/Activity_2.java
@@ -0,0 +1,15 @@
+package com.example.myfirsttask;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Activity_2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_2);
+    }
+}
